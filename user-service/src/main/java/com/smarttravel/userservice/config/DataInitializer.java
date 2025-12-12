64cbbdf59d97b621df79aab3af6dcdd250f6221d
Dilshi Wijesinghe <dilshipiyumika@gmail.com>
2025-12-12 15:00:16 +0530

    Updated user details

--- user-service/src/main/java/com/smarttravel/userservice/config/DataInitializer.java
+++ user-service/src/main/java/com/smarttravel/userservice/config/DataInitializer.java
@@ -15,8 +15,8 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        userRepository.save(new User(null, "Dilshi Piyumika", "dilshi@example.com", "0771234567"));
-        userRepository.save(new User(null, "Kasun Perera", "kasun@example.com", "0779876543"));
-        userRepository.save(new User(null, "Nimali Fernando", "nimali@example.com", "0765555555"));
+        userRepository.save(new User(null, "Dilshi Piyumika", "dilshi@gmail.com", "0771234567"));
+        userRepository.save(new User(null, "Dasun Perera", "dasun@gmail.com", "0779876543"));
+        userRepository.save(new User(null, "Nimali Fernando", "nimali@gmail.com", "0765555555"));
     }
 }
